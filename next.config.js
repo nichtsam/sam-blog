acccec0bc4c7917a257dd55fe7cd03d4c770fb97
Samuel Jensen <samuel.jensen@funpodium.net>
2022-02-19 20:20:28 +0800

    fix: contentlayer next.config.js

--- next.config.js
+++ next.config.js
@@ -5,4 +5,4 @@ const withPlugins = require('next-compose-plugins')
 const withSvgr = require('next-plugin-svgr')
 const { withContentlayer } = require('next-contentlayer')
 
-module.exports = withPlugins([withSvgr, withContentlayer], nextConfig)
+module.exports = withPlugins([withSvgr, withContentlayer()], nextConfig)
